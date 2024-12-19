ac40764e09178abe24222912915937584eeb412d
Dibas Talukder <debal1952@gmail.com>
2024-12-19 19:32:15 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#1
+
+int_set = {1, 2, 3, 4, 5}
+print(int_set)
+
+#2
+
+mixed_set = {1, 1.23, "Hello", True}
+print(mixed_set)
+
+#3
+
+my_set = {1, 2, 3, 4, 3, 2, 4}
+print(my_set)
+
+#4
+
+num_set = set([0, 1, 2, 3, 4, 5])
+
+print(num_set.pop())
